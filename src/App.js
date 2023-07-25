2cb51824ca81c0637a3a66da7a3bb08da1373624
Ridhit Garg <ridhit100@gmail.com>
2023-07-24 18:32:13 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside className='sidebar'>
+        <h1>Aside</h1>
+      </aside>
+      <section className='chatbox'>
+
+      </section>
+      
+    </div>
+  );
+}
+
+export default App;
