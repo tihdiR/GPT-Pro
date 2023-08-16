a7a80f73cba4f0f93f7f2ffec7c503880cc42736
Ridhit Garg <ridhit100@gmail.com>
2023-08-15 17:13:33 -0700

    inital layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import './normalize.css';
 
 function App() {
   return (
     <div className="App">
       <aside className='sidebar'>
-        <h1>Aside</h1>
+        <div className='new_chat_button'>
+          <span>+</span> 
+          New Chat
+        </div>
       </aside>
       <section className='chatbox'>
+        
+        <div className='prompt_input'>
+          <textarea rows={1} className='input_textarea' placeholder='Send a message'>
 
+          </textarea>
+        </div>
       </section>
       
     </div>
